763dcf52d295969d39c5d1bb6d00945e64ea2501
kuma <vityayuzbekov602@gmail.com>
2023-10-09 17:25:03 +0300

    Lab 4 complete

--- WpfApp2/WpfApp2/obj/Debug/net6.0-windows/Win5.g.i.cs
+++ WpfApp2/WpfApp2/obj/Debug/net6.0-windows/Win5.g.i.cs
@@ -53,7 +53,7 @@ namespace WpfApp2 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp2;component/win5.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp2;V1.0.0.0;component/win5.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Win5.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
